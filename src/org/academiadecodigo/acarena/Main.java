2dc8452b3e57c280a52d9e41c24495478071e9d5
Rafa <youremail@example.com>
2016-11-18 00:45:22 +0000

    nothing to commit

--- src/org/academiadecodigo/acarena/Main.java
+++ src/org/academiadecodigo/acarena/Main.java
@@ -16,6 +16,9 @@ import java.io.IOException;
  */
 public class Main {
     public static void main(String[] args) throws IOException {
+        LanternaField lanternaField = new LanternaField(10,10);
+        lanternaField.init();
+        PlayerTest playerTest = new PlayerTest(lanternaField,lanternaField.getScreen());
 
 
     }
